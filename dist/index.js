19aca6dcade7981c1c2de5a485498473eb4cf661
elbatlles <>
2021-11-25 12:42:50 +0000

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello Express app!');
+});
+app.listen(3000, () => {
+    console.log('server started');
+});
+//# sourceMappingURL=index.js.map
